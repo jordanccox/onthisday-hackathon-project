34842a438031198c28d1ae2fd941a2872eb9fdef
Jordan Cox <jordancox747@outlook.com>
2023-10-26 06:19:27 -0600

    paginateData in SearchResultsList.jsx working for Events

--- src/components/SearchResultsList.jsx
+++ src/components/SearchResultsList.jsx
@@ -1,8 +1,55 @@
-import { dummyData } from "../dummy-data"
+import { dummyData } from "../dummy-data";
+
+// Items per page: 10, 20, 50
+
+// We will need to refactor to account for filters and sorting eventually
+
+const paginateData = (input, itemsPerPage, page = 0) => {
+  const dataObj = {
+    date: input.date,
+    pages: {},
+  };
+
+  const copyLinks = (linksObject) => {
+    const copiedObject = {};
+
+    for (const link in linksObject) {
+      copiedObject[link] = {...linksObject[link]};
+    }
+
+    return copiedObject;
+  };
+
+  input.data.Events.forEach((item) => {
+    if (!dataObj.pages[page]) {
+      dataObj.pages[page] = [];
+    }
+
+    if (dataObj.pages[page].length >= itemsPerPage) {
+      page++;
+      dataObj.pages[page] = [];
+    }
+
+    const itemContents = {
+      path: `/search/${dataObj.date}/?page=${page}`,
+      text: item.text,
+      html: item.html,
+      links: copyLinks(item.links),
+    };
+
+    dataObj.pages[page].push(itemContents);
+  });
+
+  return dataObj;
+};
 
 export default function SearchResultsList() {
   const exampleText = {__html: dummyData.data.Events[0].text};
-  const exampleHTML = {__html: dummyData.data.Events[0].html}
+  const exampleHTML = {__html: dummyData.data.Events[0].html};
+
+  const examplePaginatedData = paginateData(dummyData, 10);
+
+  console.log(examplePaginatedData);
 
   return (
   <>
